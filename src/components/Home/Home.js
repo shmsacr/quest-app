02d4b0917e5cd44358c6b92effac8edc825f7a5a
shmsacr <shmsacr@gmail.com>
2023-08-11 18:03:05 +0300

    feat: create home page and pull post data

---
+++ src/components/Home/Home.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from "react";
+import Post from "../Post/Post";
+
+function Home(){
+    const [error,setError] = useState(null);
+    const [isLoaded,setIsloaded] = useState(false);
+    const [postList,setPostList] = useState([]);
+
+    useEffect(()=>{
+        fetch("/posts")
+            .then(res =>res.json())
+            .then((result) => {
+                    setIsloaded(true);
+                    setPostList(result);
+                },
+                (error) =>{
+                    setIsloaded(true);
+                    setError(error);
+                })
+    },)
+    if(error){
+        return <div> ERROR!!</div>;
+    }else if(!isLoaded){
+        return <div> Loading...</div>;
+    }else{
+        return (
+            <div className="container">
+                Home!!!
+                {postList.map(post=> (<Post title = {post.title} text ={post.text}></Post>))}
+            </div>
+
+        );
+    }
+}
+
+export default Home;
