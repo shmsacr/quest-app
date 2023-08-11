44297f22523d26e9e76b82fa786615870640ed9a
shmsacr <shmsacr@gmail.com>
2023-08-11 18:04:48 +0300

    feat: add pull data in home page

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,35 +1,10 @@
-import React,{useState,useEffect} from "react";
-import ReactDOM from "react-dom/client";
-
-function Post(){
-    const [error,setError] = useState(null);
-    const [isLoaded,setIsloaded] = useState(false);
-    const [postList,setPostList] = useState([]);
-
-    useEffect(()=>{
-        fetch("http://localhost:9090/posts")
-            .then(res =>res.json())
-            .then((result) => {
-                setIsloaded(true);
-                setPostList(result);
-                },
-                (error) =>{
-                setIsloaded(true);
-                setError(error);
-                })
-    },)
-    if(error){
-        return <div> ERROR!!</div>;
-    }else if(!isLoaded){
-        return <div> Loading...</div>;
-    }else{
-        return (
-            <u1>{postList.map(post=>(
-                <li>
-                    {post.title}{post.text}
-                </li>
-            ))}</u1>
-        );
-    }
+import React from "react";
+import "./Post.scss"
+function Post(props){
+    const {title,text} = props;
+    return <div className = "postContainer">
+        {title}
+        {text}
+    </div>
 }
 export default Post;
