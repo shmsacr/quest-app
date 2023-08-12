514fae3cad70c76f3607b469efd05b25362bff4e
shmsacr <shmsacr@gmail.com>
2023-08-12 18:49:39 +0300

    feat: config Link method

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,37 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { styled } from '@mui/system';
 
-function NavBar(){
-    let userId =5;
-    return <div>
-        <ul>
-            <li><Link to = "/">Home</Link></li>
-            <li><Link to = {{pathname :'/users/' + userId}}>User</Link></li>
-        </ul>
-    </div>
+const MyLink = styled(Link)({
+    textDecoration: "none",
+    boxShadow: "none",
+    color: "white"
+});
+
+
+function Navbar() {
+    let userId = 5;
+
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton><Typography variant = "h6" sx={{ flexGrow: 1, textAlign:"left"}} color ="inherit" aria-label = "menu">
+                        <MyLink to="/">Home</MyLink></Typography>
+                    <Typography variant = "h6" color ="inherit" >
+                        <MyLink to={{ pathname: '/users/' + userId }}>User</MyLink></Typography>
+
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
 }
 
-export default NavBar;
+export default Navbar;
